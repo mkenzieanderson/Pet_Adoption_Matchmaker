7a75c1bb99710758b288a08b045f8a677a62cb66
Astrid Bowden <137099956+astridlotus@users.noreply.github.com>
2025-01-26 11:18:23 -0800

    add seed_data file to put test records in db. add login endpoint testing and working. update requirements for bcrypt

--- backend/main.py
+++ backend/main.py
@@ -16,12 +16,36 @@ from __future__ import annotations
 
 import logging
 import os
+import json
+import requests
+import sqlalchemy
+import bcrypt
+
+from dotenv import load_dotenv
+from six.moves.urllib.request import urlopen
+from jose import jwt
+from authlib.integrations.flask_client import OAuth
+from flask import Flask, request, jsonify, send_file
+
 
 from flask import Flask, request, jsonify
 
-import sqlalchemy
 
+load_dotenv()
+
+# Global endpoint names
+PETS = "pets"
+PET_DISPOSITIONS  = 'pet_dispositions'
+SHELTERS = "shelters"
+FAVORITES = "favorites"
 USERS = "users"
+
+PET_PIC_BUCKET = "cap_pet_photos"
+
+#global secrets for auth
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+DOMAIN = os.getenv("DOMAIN")
 
 from connect_connector import connect_with_connector
 
@@ -54,9 +78,9 @@ def create_table(db: sqlalchemy.engine.base.Engine) -> None:
         # Create the users table
         conn.execute(
             sqlalchemy.text(
-               '''
-                CREATE TABLE IF NOT EXISTS users (
+               '''CREATE TABLE IF NOT EXISTS users (
                     user_id BIGINT AUTO_INCREMENT NOT NULL,
+                    sub VARCHAR(255) UNIQUE,
                     email VARCHAR(255) NOT NULL UNIQUE,
                     password_hash VARCHAR(255) NOT NULL,
                     phone_number VARCHAR(15),
@@ -136,10 +160,55 @@ def create_table(db: sqlalchemy.engine.base.Engine) -> None:
 
         conn.commit()
 
+def get_error_message(status_code):
+    """Return a JSON error message for a given response status code."""
+    error_messages = {
+        400: {"Error": "The request body is invalid"},
+        401: {"Error": "Unauthorized"},
+        403: {"Error": "You don't have permission on this resource"},
+        404: {"Error": "Not found"},
+        409: {"Error": "data is invalid"}
+    }
+    return error_messages.get(status_code, {"Error": "Unknown error code"})
 
 @app.route('/')
 def index():
     return 'Please navigate to /pets to use this API'
+
+@app.route('/'+ USERS + '/login', methods=['POST'])
+def login_user():
+    try:
+        content = request.get_json()
+
+        if not content or not all(key in content for key in ["email", "password"]):
+            raise ValueError(400)
+
+        email = content["email"]
+        password = content["password"]
+        print(email)
+        print(password)
+
+        body = {'grant_type':'password',
+                'username':email, #Auth0 Setup with username. Using email address as username
+                'password':password,
+                'client_id':CLIENT_ID,
+                'client_secret':CLIENT_SECRET
+                }
+
+        headers = { 'content-type': 'application/json' }
+        url = 'https://' + DOMAIN + '/oauth/token'
+
+        r = requests.post(url, json=body, headers=headers)
+
+        if r.status_code != 200:
+            raise ValueError(401)
+        token = r.json().get('id_token')
+        return jsonify({"token": token}), 200
+
+    except ValueError as e:
+        status_code = int(str(e))
+        return get_error_message(status_code), status_code
+
 
 
 if __name__ == '__main__':
