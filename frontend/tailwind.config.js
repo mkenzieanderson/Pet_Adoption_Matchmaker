3ff7a2d29acc24489c414853db014a3fa916a231
Mackenzie Anderson <mackenzieanderson@outlook.com>
2025-01-28 20:24:26 -0800

    added custom colors and fonts from the Figma

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'espresso': '#3d2c04',      /* all other text */ 
+        'dark-cherry': '#47000e',   /* error text, error button outline */
+        'beige': '#f8f3e6',         /* website whitespace */ 
+        'mustard': '#bd912987',     /* navigation bar, buttons, etc. */ 
+        'tawny-brown': '#755509',   /* container and button outlines */
+        'cream': '#fffdf7',         /* forms */ 
+        'light-rose': '#d775895e'   /* error and buttons */ 
+      },
+      fontFamily: {
+        header: ['Noto Serif'],     /* Headers, Titles, Larger Body Text */
+        body: ['Noto Sans']         /* Small Body Text */       
+      }
+    },
   },
   plugins: [],
 }
