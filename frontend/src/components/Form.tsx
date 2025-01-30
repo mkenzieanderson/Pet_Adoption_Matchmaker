9c46d9aa20754cb9b8ff7f213cb38ecab8dfc16e
Mackenzie Anderson <mackenzieanderson@outlook.com>
2025-01-29 19:49:37 -0800

    Sign In Page Basic Frontend

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -4,12 +4,13 @@ type FormProps = {
     title?: string;
     description?: string;
     children: ReactNode;
+    width?: string;
 }
 
-function Form ({ title, description, children }: FormProps) {
+function Form ({ title, description, children, width }: FormProps) {
     return (
         <div className={`font-header text-espresso bg-cream border-tawny-brown 
-                        border-2 rounded-lg p-8 pt-10 pb-12`}>
+                        border-2 rounded-lg p-12 pt-10 pb-12 ${width}`}>
             {title && <h2 className="font-bold text-3xl mb-1">{title}</h2>}
             {description && <p className="mb-8">{description}</p>}
 
