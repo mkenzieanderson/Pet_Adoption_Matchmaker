82342ead9d4be06545a130687bc43205d1310a41
Mackenzie Anderson <mackenzieanderson@outlook.com>
2025-01-31 14:54:04 -0800

    refactored code for shorter files

---
+++ frontend/src/styles/ButtonStyles.tsx
@@ -0,0 +1,13 @@
+export const defaultButtonStyle =   `bg-mustard text-espresso font-header 
+                                    font-semibold border-tawny-brown border-4
+                                    rounded-lg px-6 py-2 mt-4 mb-2 w-auto
+                                    hover:border-espresso hover:bg-transparent-clay
+                                    focus:border-espresso focus:bg-transparent-clay
+                                    focus:outline-espresso`;
+
+export const errorButtonStyle =    `bg-light-rose text-dark-cherry font-header 
+                                    font-semibold border-dark-cherry border-4
+                                    rounded-lg px-6 py-2 mt-4 mb-2 w-auto
+                                    hover:border-dark-cherry hover:bg-dark-rose
+                                    focus:border-dark-cherry focus:bg-dark-rose
+                                    focus:outline-dark-cherry`;
