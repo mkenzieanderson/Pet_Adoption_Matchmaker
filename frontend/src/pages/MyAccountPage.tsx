92d7e3b352f27d5ca7a3789724024fe8125503ac
Mackenzie Anderson <mackenzieanderson@outlook.com>
2025-02-09 13:50:49 -0800

    added header comp to my account page

--- frontend/src/pages/MyAccountPage.tsx
+++ frontend/src/pages/MyAccountPage.tsx
@@ -1,28 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import Header from "../components/Header/Header";
+import { dummyUser } from "../state/User.types";
+import Button from "../components/Buttons/Button";
+import TextInput from "../components/TextInput/TextInput";
+import Form from "../components/Form/Form";
 
 export const MyAccountPage = () => {
-    const navigate = useNavigate();
 
     return (
         <>
-            <div className=" rounded-lg p-4">
-                <h1>My Account Page</h1>
-                <button onClick={() => navigate('/')} className="m-4">
-                    HomePage
-                </button>
-                <button onClick={() => navigate('/shelter-page')} className="m-4">
-                    ShelterPage
-                </button>
-                <button onClick={() => navigate('/pets-page')} className="m-4">
-                    Pets Page
-                </button>
-                <button onClick={() => navigate('/add-pet-page')} className="m-4">
-                    Add Pet Page
-                </button>
-                <button onClick={() => navigate('/edit-pet-page')} className="m-4">
-                    Edit Pet Page
-                </button>
-            </div>
+            <Header user={dummyUser} path={location.pathname} loginStatus={true} />
         </>
     )
 }
