48cb98da38401034da6b185aff42dcec373b7163
Mackenzie Anderson <mackenzieanderson@outlook.com>
2025-03-05 14:48:21 -0800

    URL is running server URL instead of local host

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,10 @@ import { EditPetPage } from './pages/EditPetPage'
 import { AddPetPage } from './pages/AddPetPage'
 import './App.css'
 
-// export const URL = import.meta.env.VITE_BASE_URL
+export const URL = import.meta.env.VITE_BASE_URL
 
 // For development purposes until bugs with the import.meta.env.VITE_BASE_URL are resolved
-export const URL = 'http://localhost:8080/';
+// export const URL = 'http://localhost:8080/';
 
 const routes = createBrowserRouter([
   {path: '/', element: <HomePage />},
